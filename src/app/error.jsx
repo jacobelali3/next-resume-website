d34cf020d17bab1c7adf242dcf4550d51c9f0008
Jacob <jacobelali3@outlook.com>
2023-03-10 00:02:22 +1100

    changed error page

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -19,9 +19,9 @@ export default function Error({ error, reset }) {
       height={1000}
     />
     <div className="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
-      <p className="text-base font-semibold leading-8 text-white">404</p>
-      <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">Page not found</h1>
-      <p className="mt-4 text-base text-white/70 sm:mt-6">Sorry, seems my website pooped itself!.<br></br>But its a 404 so its likely a you problem :D</p>
+      <p className="text-base font-semibold leading-8 text-white">Error</p>
+      <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">beep bop boop</h1>
+      <p className="mt-4 text-base text-white/70 sm:mt-6">Sorry, seems my website pooped itself!.</p>
       <div className="mt-10 flex justify-center">
         <a href="/" className="text-sm font-semibold leading-7 text-white">
           <span aria-hidden="true">&larr;</span> Back to home
