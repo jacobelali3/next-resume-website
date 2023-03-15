211b5d90dadc9832edda51b167e484e4f6d5e163
Jacob <jacobelali3@outlook.com>
2023-03-16 03:08:30 +1100

    fixed button

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,12 +8,12 @@ export default function Home() {
     <Video></Video>
 
     <div  className=" h-22 mt-48 flex justify-center mx-12">
+    <Link href="/about">
       <button className="px-6 py-4 relative bg-slate-800 rounded-lg animate-beat ">
-
         <h1 className="mb-4 text-2xl text-white text-center">Welcome,<br></br>I&apos;m a software engineer with a passion for technology!</h1>
-        <Link className='text-white text-center no-underline hover:underline decoration-blue-400' href="/about">Check me out!</Link>
+        <p className='text-white text-center no-underline hover:underline decoration-blue-400'>Check me out!</p>
         </button>
-
+        </Link>
     </div>
 
     </div>
