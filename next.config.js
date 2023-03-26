fd1afa21bacee811b95c4da8c25f2a880f39a922
Jacob <jacobelali3@outlook.com>
2023-03-27 04:15:54 +1100

    Sack using next/image for dynamic images

--- next.config.js
+++ next.config.js
@@ -3,9 +3,7 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  images: {
-    domains: ['https://d1jt5eir8qi0yi.cloudfront.net'],
-  }
+
 }
 
 
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,10 +1,5 @@
 import PocketBase from "pocketbase";
 import Link from "next/link";
-import Image from "next/image";
-
-export default function myLoader({ src }) {
-  return `${src}`
-}
 
 async function getBlogs() {
   const pb = new PocketBase(process.env.BACKEND);
@@ -45,13 +40,10 @@ export default async function Blog() {
                 key={post.id}
                 className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80"
               >
-                <Image
-                  loader={myLoader}
+                <img
                   src={post.imageUrl}
                   alt="Blog intro image"
                   className="absolute inset-0 -z-10 h-full w-full object-cover"
-                  width={200}
-                  height={200}
                 />
                 <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
                 <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
