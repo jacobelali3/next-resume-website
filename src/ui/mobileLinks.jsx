c7103265b83012eae65c0e106354515cd407f216
Jacob <jacobelali3@outlook.com>
2023-03-27 04:21:45 +1100

    add blog to mobile menu

--- src/ui/mobileLinks.jsx
+++ src/ui/mobileLinks.jsx
@@ -23,6 +23,9 @@ function MobileNav() {
           <p className=" mt-4  text-white hover:text-blue-300 ">
           <Link href="/projects">Projects</Link>
           </p>
+          <p className=" mt-4  text-white hover:text-blue-300 ">
+          <Link href="/blog">Blog</Link>
+          </p>
           <p className="mt-4 px-4 py-1 border rounded text-white border-white hover:border-transparent hover:text-blue-300 hover:bg-white">
             <a
               href={"mailto:jacobelali3@outlook.com"}
